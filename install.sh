da857cd963e5fbaf3b47f8d6e37287ceb5f49655
Ananth Mahadevan <ananth.mahadevan@helsinki.fi>
2022-07-13 14:55:58 +0300

    Adds script to install fzf and rust crates

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+##############
+# FZF
+##############
+
+if [[ ! -f $HOME/.fzf/bin/fzf ]]; then
+    git clone https://github.com/junegunn/fzf.git $HOME/.fzf
+    yes | $HOME/.fzf/install
+fi
+
+###############
+# RUST
+##############
+
+if [[ ! -d $HOME/.rustup ]]; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+
+for crate in bat fd-find ripgrep exa du-dust ripgrep git-delta starship zoxide
+do
+    $HOME/.cargo/bin/cargo install $crate
+done
